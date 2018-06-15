488a4d237242bb2d8dea067b5cc6d42e8d911ad3
victor.chicu <victor.chicu@hotmail.com>
2018-06-15 22:14:22 +0300

    Egghead: Fixed returnUrl

--- Egghead/Controllers/AccountController.cs
+++ Egghead/Controllers/AccountController.cs
@@ -132,7 +132,7 @@ namespace Egghead.Controllers
 
             return Ok(new ErrorViewModel
             {
-                RedirectUrl = "/",
+                RedirectUrl = returnUrl,
                 ErrorStatusCode = ErrorStatusCode.Found
             });
         }
@@ -241,7 +241,7 @@ namespace Egghead.Controllers
 
             return Ok(new ErrorViewModel
             {
-                RedirectUrl = "/",
+                RedirectUrl = returnUrl,
                 ErrorStatusCode = ErrorStatusCode.Found
             });
         }
