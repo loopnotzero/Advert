8045dddcfeda6a19e71b5fb6fa08401477d13a4b
victor.chicu <victor.chicu@hotmail.com>
2018-06-30 18:18:27 +0300

    Egghead.Common: Added OperationResult for some methods execution result

---
+++ Egghead.Common/OperationError.cs
@@ -0,0 +1,13 @@
+﻿namespace Egghead.Common
+{
+    public class OperationError
+    {
+        /// <summary>Gets or sets the code for this error.</summary>
+        /// <value>The code for this error.</value>
+        public string Code { get; set; }
+
+        /// <summary>Gets or sets the description for this error.</summary>
+        /// <value>The description for this error.</value>
+        public string Description { get; set; }
+    }
+}
