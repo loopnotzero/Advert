2f20ad23dd8e3de63c0236628e29f9c64d7d481f
victor.chicu <victor.chicu@hotmail.com>
2018-06-30 16:16:22 +0300

    Egghead: Implemented ArticlesLikesManager, ArticlesViewsManager
    Added count for likes, unlikes, views e.t.c.

--- Egghead/Managers/ArticlesLikesManager.cs
+++ Egghead/Managers/ArticlesLikesManager.cs
@@ -1,7 +1,97 @@
-﻿namespace Egghead.Managers
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Egghead.Common.Articles;
+using Egghead.MongoDbStorage.IStores;
+using MongoDB.Driver;
+
+namespace Egghead.Managers
 {
-    public class LikesManager
+    public class ArticlesLikesManager<T> : IDisposable where T : class
     {
-        
+        private bool _disposed;
+
+        /// <summary>
+        /// Gets or sets the persistence store the manager operates over.
+        /// </summary>
+        /// <value>The persistence store the manager operates over.</value>
+        protected internal IArticlesLikesStore<T> Store { get; set; }
+
+        public ArticlesLikesManager(IArticlesLikesStore<T> store)
+        {
+            Store = store ?? throw new ArgumentNullException(nameof(store));
+        }
+
+        public async Task<T> FindArticlesLikesByArticleIdAsync(string articleId, CancellationToken cancellationToken)
+        {
+            ThrowIfDisposed();
+
+            if (articleId == null)
+            {
+                throw new ArgumentNullException(nameof(articleId));
+            }
+
+            return await Store.FindArticlesLikesByArticleIdAsync(articleId, cancellationToken);
+        }
+
+        public async Task<T> FindArticlesUnlikesByArticleIdAsync(string articleId, CancellationToken cancellationToken)
+        {
+            ThrowIfDisposed();
+
+            if (articleId == null)
+            {
+                throw new ArgumentNullException(nameof(articleId));
+            }
+
+            return await Store.FindArticlesUnlikesByArticleIdAsync(articleId, cancellationToken);
+        }
+
+        public async Task<long> CountArticlesLikesByArticleIdAsync(string articleId, CancellationToken cancellationToken)
+        {
+            ThrowIfDisposed();
+
+            if (articleId == null)
+            {
+                throw new ArgumentNullException(nameof(articleId));
+            }
+
+            return await Store.CountArticlesLikesByArticleIdAsync(articleId, cancellationToken);
+        }
+
+        public async Task<long> CountArticlesUnlikesByArticleIdAsync(string articleId, CancellationToken cancellationToken)
+        {
+            ThrowIfDisposed();
+
+            if (articleId == null)
+            {
+                throw new ArgumentNullException(nameof(articleId));
+            }
+
+            return await Store.CountArticlesUnlikesByArticleIdAsync(articleId, cancellationToken);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!disposing || _disposed)
+                return;
+
+            Store.Dispose();
+
+            _disposed = true;
+        }
+
+        protected void ThrowIfDisposed()
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException(GetType().Name);
+            }
+        }
     }
 }
--- Egghead/Managers/ArticlesViewsManager.cs
+++ Egghead/Managers/ArticlesViewsManager.cs
@@ -1,7 +1,72 @@
-﻿namespace Egghead.Managers
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Egghead.MongoDbStorage.IStores;
+using MongoDB.Driver;
+
+namespace Egghead.Managers
 {
-    public class ArticlesViewsManager
+    public class ArticlesViewsManager<T> : IDisposable where T : class
     {
+        private bool _disposed;
         
+        /// <summary>
+        /// Gets or sets the persistence store the manager operates over.
+        /// </summary>
+        /// <value>The persistence store the manager operates over.</value>
+        protected internal IArticlesViewsStore<T> Store { get; set; }
+
+        public ArticlesViewsManager(IArticlesViewsStore<T> store)
+        {
+            Store = store ?? throw new ArgumentNullException(nameof(store));
+        }
+        
+        public async Task<T> FindArticlesViewsByArticleIdAsync(string articleId, CancellationToken cancellationToken)
+        {
+            ThrowIfDisposed();
+
+            if (articleId == null)
+            {
+                throw new ArgumentNullException(nameof(articleId));
+            }
+
+            return await Store.FindArticlesViewsByArticleIdAsync(articleId, cancellationToken);
+        }
+
+        public async Task<long> CountArticlesViewsByArticleIdAsync(string articleId, CancellationToken cancellationToken)
+        {
+            ThrowIfDisposed();
+
+            if (articleId == null)
+            {
+                throw new ArgumentNullException(nameof(articleId));
+            }
+
+            return await Store.CountArticlesViewsByArticleIdAsync(articleId, cancellationToken);
+        }
+        
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!disposing || _disposed)
+                return;
+
+            Store.Dispose();
+
+            _disposed = true;
+        }
+
+        protected void ThrowIfDisposed()
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException(GetType().Name);
+            }
+        }
     }
 }
