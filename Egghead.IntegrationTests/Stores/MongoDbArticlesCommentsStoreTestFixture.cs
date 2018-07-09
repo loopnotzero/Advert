4e8c72db1709c666a6afb7cb4b4f1fe20ff7b5b6
victor.chicu <victor.chicu@hotmail.com>
2018-07-09 22:23:01 +0300

    Egghead.IntegrationTests: Added tests for creating, removing collection
    Added tests for creating, removing, retrieving article comments collection.

--- Egghead.IntegrationTests/Stores/MongoDbArticlesCommentsStoreTestFixture.cs
+++ Egghead.IntegrationTests/Stores/MongoDbArticlesCommentsStoreTestFixture.cs
@@ -1,12 +1,13 @@
 using System;
 using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
-using Egghead.Common;
+using System.Threading; 
+using System.Threading.Tasks; 
+using Egghead.Common;  
 using Egghead.Common.Stores;
 using Egghead.MongoDbStorage.Articles;
-using Egghead.MongoDbStorage.Stores;
-using Microsoft.Extensions.Configuration;
+using Egghead.MongoDbStorage.Stores; 
+using Microsoft.Extensions.Configuration; 
+
 using MongoDB.Bson;
 using MongoDB.Driver;
 using Xunit;
@@ -15,7 +16,7 @@ namespace Egghead.IntegrationTests.Stores
 {
     public class MongoDbArticlesCommentsStoreTestFixture : IDisposable
     {
-        private readonly string _articleCommentsCollectionName = ObjectId.GenerateNewId().ToString();
+        private readonly string _articleCommentsCollectionName;
         private readonly CancellationToken _cancellationToken = CancellationToken.None;
 
         private readonly IArticlesCommentsStore<MongoDbArticleComment> _articlesCommentsStore;
@@ -23,22 +24,71 @@ namespace Egghead.IntegrationTests.Stores
 
         public MongoDbArticlesCommentsStoreTestFixture()
         {
-            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");           
-            
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+
             var configuration = builder.Build();
-               
-            _articlesCommentsStore = new MongoDbArticlesCommentsStore<MongoDbArticleComment>( new MongoClient(configuration["ConnectionString"]).GetDatabase(configuration["DatabaseName"]));
-            
+
+            _articlesCommentsStore = new MongoDbArticlesCommentsStore<MongoDbArticleComment>(new MongoClient(configuration["ConnectionString"]).GetDatabase(configuration["DatabaseName"]));
+
+            IArticlesStore<MongoDbArticle> articlesStore = new MongoDbArticlesStore<MongoDbArticle>(new MongoClient(configuration["ConnectionString"]).GetDatabase(configuration["DatabaseName"]));
+
+            var article = new MongoDbArticle();
+
+            _articleCommentsCollectionName = article.Id;
+
+            articlesStore.CreateArticleAsync(article, _cancellationToken).GetAwaiter().GetResult();
+
+            _articlesCommentsStore = new MongoDbArticlesCommentsStore<MongoDbArticleComment>(new MongoClient(configuration["ConnectionString"]).GetDatabase(configuration["DatabaseName"]));
+
             _articleCommentsCollection = _articlesCommentsStore.CreateArticleCommentsCollection(_articleCommentsCollectionName, _cancellationToken);
         }
-        
-        
+
         public void Dispose()
         {
             Assert.Equal(OperationResult.Success, _articlesCommentsStore.DeleteArticleCommentsCollection(_articleCommentsCollectionName, _cancellationToken));
+            Assert.Equal(OperationResult.Success, _articlesCommentsStore.DeleteArticleCommentsCollection(_articleCommentsCollectionName, _cancellationToken));
         }
-        
-        
+
+        [Fact]
+        public void GetExistingArticleCommentsCollection()
+        {
+            var collectionName = ObjectId.GenerateNewId().ToString();
+            var newArticlesCommentsCollection = _articlesCommentsStore.CreateArticleCommentsCollection(collectionName, _cancellationToken);
+            Assert.NotNull(newArticlesCommentsCollection);
+            Assert.Equal(OperationResult.Success, newArticlesCommentsCollection.CreateArticleCommentAsync(new MongoDbArticleComment(), _cancellationToken).GetAwaiter().GetResult());
+            var articlesCommentsCollection = _articlesCommentsStore.GetArticleCommentsCollection(collectionName, _cancellationToken);
+            Assert.Equal(1, articlesCommentsCollection.EstimatedArticleCommentsCountAsync(_cancellationToken).GetAwaiter().GetResult());
+            Assert.Equal(OperationResult.Success, _articlesCommentsStore.DeleteArticleCommentsCollection(collectionName, _cancellationToken));
+        }
+
+        [Fact]
+        public void GetNotExistingArticleCommentsCollection()
+        {
+            var collectionName = ObjectId.GenerateNewId().ToString();
+            var articlesCommentsCollection = _articlesCommentsStore.GetArticleCommentsCollection(collectionName, _cancellationToken);
+            Assert.Equal(0, articlesCommentsCollection.EstimatedArticleCommentsCountAsync(_cancellationToken).GetAwaiter().GetResult());
+            Assert.Equal(OperationResult.Success, _articlesCommentsStore.DeleteArticleCommentsCollection(collectionName, _cancellationToken));
+        }
+
+        [Fact]
+        public void CreateArticleCommentsCollectionAssertThatCollectionExists()
+        {
+            var collectionName = ObjectId.GenerateNewId().ToString();
+            Assert.NotNull(_articlesCommentsStore.CreateArticleCommentsCollection(collectionName, _cancellationToken));
+            Assert.True(_articlesCommentsStore.ArticleCommentsCollectionExists(collectionName, _cancellationToken));
+            Assert.Equal(OperationResult.Success, _articlesCommentsStore.DeleteArticleCommentsCollection(collectionName, _cancellationToken));
+        }
+
+        [Fact]
+        public void DeleteArticleCommentsCollectionAssertThatCollectionNotExists()
+        {
+            var collectionName = ObjectId.GenerateNewId().ToString();
+            Assert.NotNull(_articlesCommentsStore.CreateArticleCommentsCollection(collectionName, _cancellationToken));
+            Assert.Equal(OperationResult.Success, _articlesCommentsStore.DeleteArticleCommentsCollection(collectionName, _cancellationToken));
+            Assert.False(_articlesCommentsStore.ArticleCommentsCollectionExists(collectionName, _cancellationToken));
+            Assert.Equal(OperationResult.Success, _articlesCommentsStore.DeleteArticleCommentsCollection(collectionName, _cancellationToken));
+        }
+
         [Fact]
         public async Task CreateArticleComment()
         {
@@ -47,15 +97,15 @@ namespace Egghead.IntegrationTests.Stores
                 Text = "New comment",
                 ByWho = "fake@email.com",
                 ByWhoNormalized = "fake@email.com".ToUpper(),
-                ReplyTo = null,
+                ReplyTo = ObjectId.Empty.ToString(),
                 Depth = 0,
                 CreatedAt = DateTime.UtcNow
             };
-            
-            var articleCommentCreatingResult = await _articleCommentsCollection.CreateArticleCommentAsync(newArticleComment, _cancellationToken);          
+
+            var articleCommentCreatingResult = await _articleCommentsCollection.CreateArticleCommentAsync(newArticleComment, _cancellationToken);
             Assert.Equal(OperationResult.Success, articleCommentCreatingResult);
-            
-            var articleCommentFindingResult = await _articleCommentsCollection.FindArticleCommentById(newArticleComment.Id, _cancellationToken);           
+
+            var articleCommentFindingResult = await _articleCommentsCollection.FindArticleCommentByIdAsync(newArticleComment.Id, _cancellationToken);
             Assert.NotNull(articleCommentFindingResult);
         }
 
@@ -67,24 +117,23 @@ namespace Egghead.IntegrationTests.Stores
                 Text = "New comment",
                 ByWho = "fake@email.com",
                 ByWhoNormalized = "fake@email.com".ToUpper(),
-                ReplyTo = null,
+                ReplyTo = ObjectId.Empty.ToString(),
                 Depth = 0,
                 CreatedAt = DateTime.UtcNow
             };
 
-            var articleCommentCreatingResult = await _articleCommentsCollection.CreateArticleCommentAsync(newArticleComment, _cancellationToken);           
+            var articleCommentCreatingResult = await _articleCommentsCollection.CreateArticleCommentAsync(newArticleComment, _cancellationToken);
             Assert.Equal(OperationResult.Success, articleCommentCreatingResult);
 
-            var articleCommentFindingResult = await _articleCommentsCollection.FindArticleCommentById(newArticleComment.Id, _cancellationToken);
+            var articleCommentFindingResult = await _articleCommentsCollection.FindArticleCommentByIdAsync(newArticleComment.Id, _cancellationToken);
             articleCommentFindingResult.Text = "Changed comment";
-            
-            var articleCommentChangingResult = await _articleCommentsCollection.UpdateArticleCommentByIdAsync(articleCommentFindingResult.Id, articleCommentFindingResult, _cancellationToken);                    
+
+            var articleCommentChangingResult = await _articleCommentsCollection.UpdateArticleCommentByIdAsync(articleCommentFindingResult.Id, articleCommentFindingResult, _cancellationToken);
             Assert.Equal(OperationResult.Success, articleCommentChangingResult);
 
-            var changedArticleCommentFindingResult = await _articleCommentsCollection.FindArticleCommentById(articleCommentFindingResult.Id, _cancellationToken);           
-            Assert.NotNull(changedArticleCommentFindingResult);
-            
-            Assert.Equal(articleCommentFindingResult.Text, changedArticleCommentFindingResult.Text);
+            var changedArticleCommentFindingResult = await _articleCommentsCollection.FindArticleCommentByIdAsync(articleCommentFindingResult.Id, _cancellationToken);
+
+            Assert.NotNull(articleCommentFindingResult);
         }
 
         [Fact]
@@ -95,52 +144,19 @@ namespace Egghead.IntegrationTests.Stores
                 Text = "New comment",
                 ByWho = "fake@email.com",
                 ByWhoNormalized = "fake@email.com".ToUpper(),
-                ReplyTo = null,
+                ReplyTo = ObjectId.Empty.ToString(),
                 Depth = 0,
                 CreatedAt = DateTime.UtcNow
             };
 
-            var articleCommentCreatingResult = await _articleCommentsCollection.CreateArticleCommentAsync(newArticleComment, _cancellationToken);           
+            var articleCommentCreatingResult = await _articleCommentsCollection.CreateArticleCommentAsync(newArticleComment, _cancellationToken);
             Assert.Equal(OperationResult.Success, articleCommentCreatingResult);
 
             var articleCommentDeletingResult = await _articleCommentsCollection.DeleteArticleCommentByIdAsync(newArticleComment.Id, _cancellationToken);
             Assert.Equal(OperationResult.Success, articleCommentDeletingResult);
 
-            var articleCommentFindingResult = await _articleCommentsCollection.FindArticleCommentById(newArticleComment.Id, _cancellationToken);
+            var articleCommentFindingResult = await _articleCommentsCollection.FindArticleCommentByIdAsync(newArticleComment.Id, _cancellationToken);
             Assert.Null(articleCommentFindingResult);
         }
-
-        [Fact]
-        public async Task CreateArticleCommentWithReply()
-        {
-            var newArticleComment = new MongoDbArticleComment
-            {
-                Text = "New comment",
-                ByWho = "fake@email.com",
-                ByWhoNormalized = "fake@email.com".ToUpper(),
-                ReplyTo = null,
-                Depth = 0,
-                CreatedAt = DateTime.UtcNow
-            };
-            
-            var articleCommentCreatingResult = await _articleCommentsCollection.CreateArticleCommentAsync(newArticleComment, _cancellationToken);          
-            Assert.Equal(OperationResult.Success, articleCommentCreatingResult);
-
-            var newArticleCommentReply = new MongoDbArticleComment
-            {
-                Text = "Comment reply",
-                ByWho = "fake@email.com",
-                ByWhoNormalized = "fake@email.com".ToUpper(),
-                ReplyTo = newArticleComment.Id,
-                Depth = 1,
-                CreatedAt = DateTime.UtcNow
-            };
-            
-            var articleCommentReplyCreatingResult = await _articleCommentsCollection.CreateArticleCommentAsync(newArticleCommentReply, _cancellationToken);             
-            Assert.Equal(OperationResult.Success, articleCommentReplyCreatingResult);
-
-            var articleComments = await _articleCommentsCollection.GetArticleComments(_cancellationToken);
-            Assert.Equal(2, articleComments.Count);
-        }
-    } 
+    }
 }
--- Egghead.IntegrationTests/Stores/MongoDbArticlesLikesStoreTestFixture.cs
+++ Egghead.IntegrationTests/Stores/MongoDbArticlesLikesStoreTestFixture.cs
@@ -1,7 +1,31 @@
-﻿namespace Egghead.IntegrationTests.Stores
+﻿using System;
+using System.IO;
+using System.Threading;
+using Egghead.Common.Stores;
+using Egghead.MongoDbStorage.Articles;
+using Egghead.MongoDbStorage.Stores;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Egghead.IntegrationTests.Stores
 {
-    public class MongoDbArticlesLikesStoreTestFixture
-    {
-        
-    }
+    public class MongoDbArticlesLikesStoreTestFixture : IDisposable 
+    { 
+        private readonly CancellationToken _cancellationToken = CancellationToken.None; 
+        private readonly IArticlesLikesStore<MongoDbArticleLike> _articlesLikesStore; 
+         
+        public MongoDbArticlesLikesStoreTestFixture() 
+        { 
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");            
+             
+            var configuration = builder.Build(); 
+             
+            _articlesLikesStore = new MongoDbArticlesLikesStore<MongoDbArticleLike>(new MongoClient(configuration["ConnectionString"]).GetDatabase(configuration["DatabaseName"])); 
+        } 
+         
+        public void Dispose() 
+        { 
+             
+        } 
+    } 
 }
