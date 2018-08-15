02043a0659a091aa67eb03b50736dec9b0f8cb47
victor.chicu <victor.chicu@hotmail.com>
2018-08-16 00:03:54 +0300

    Egghead.Common: Added article comment like interface

---
+++ Egghead.Common/Stores/IArticleCommentsLikesStore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Egghead.Common.Stores
+{
+    public interface IArticleCommentsLikesStore<T> : IDisposable where T : class
+    {
+        Task<T> FindArticleCommentLikesByArticleIdAsync(string articleId, CancellationToken cancellationToken);
+        Task<T> FindArticleCommentDislikesByArticleIdAsync(string articleId, CancellationToken cancellationToken);
+        Task<long> CountArticleCommentLikesByArticleIdAsync(string articleId, CancellationToken cancellationToken);
+        Task<long> CountArticleCommentDislikesByArticleIdAsync(string articleId, CancellationToken cancellationToken);
+        Task<OperationResult> SetArticleCommentLikeAsync(T entity , CancellationToken cancellationToken);
+    }
+}
