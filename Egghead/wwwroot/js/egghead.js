c02e446c3ad3bd34bf265fbe4b581f39c9fc87b6
victor.chicu <victor.chicu@hotmail.com>
2018-08-29 23:28:00 +0300

    Egghead: Partial switch to material admin 2.0

--- Egghead/wwwroot/js/egghead.js
+++ Egghead/wwwroot/js/egghead.js
@@ -4,22 +4,16 @@ $(document).ready(function () {
         form.each(function () {
             var p = $(this).find('.form-control').val();
             if (p.length !== 0) {
-                console.log("Form control is not empty 1");
                 $(this).find('.form-control').addClass('form-control--active');
-            } else {
-                console.log("Form control is empty 1");
-                // $(this).find('.form-control').addClass('form-control--active');
             }
         });
 
         $('body').on('blur', '.form-group--float .form-control', function () {
             var i = $(this).val();
             if (i.length === 0) {
-                console.log("Form control is empty 2");
                 $(this).removeClass('form-control--active');
             }
             else {
-                console.log("Form control is not empty 2");
                 $(this).addClass('form-control--active');
             }
         });
@@ -27,7 +21,6 @@ $(document).ready(function () {
         $(this).find('.form-control').change(function () {
             var x = $(this).val();
             if (x.length !== 0) {
-                console.log("Form control is not empty 3");
                 $(this).find('.form-control').addClass('form-control--active');
             }
         });
