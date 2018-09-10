6dd6aba64ca9b8dd5ad0aa6fe0c95d36801fd31a
victor.chicu <victor.chicu@hotmail.com>
2018-09-11 01:10:26 +0300

    Egghead: Added small example with uploading files to profiles controller

--- Egghead/Controllers/ProfilesController.cs
+++ Egghead/Controllers/ProfilesController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace Egghead.Controllers
@@ -16,6 +21,31 @@ namespace Egghead.Controllers
         public IActionResult GetProfile()
         {
             return View();
+        }
+        
+        [HttpPost("UploadFiles")]
+        public async Task<IActionResult> Post(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            // full path to file in temp location
+            var filePath = Path.GetTempFileName();
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { count = files.Count, size, filePath});
         }
     }
 }
