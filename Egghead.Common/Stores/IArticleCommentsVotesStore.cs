617d144a785869b84706beb5e214e9e7e0246890
victor.chicu <victor.chicu@hotmail.com>
2018-09-22 01:32:29 +0300

    Egghead.Common: Removed redundant params from article comments votes.

--- Egghead.Common/Stores/IArticleCommentsVotesStore.cs
+++ Egghead.Common/Stores/IArticleCommentsVotesStore.cs
@@ -8,8 +8,8 @@ namespace Egghead.Common.Stores
 {
     public interface IArticleCommentsVotesStore<T> : IDisposable where T : class
     {
-        Task<T> FindArticleCommentVoteAsync(ObjectId articleId, ObjectId commentId, CancellationToken cancellationToken);
-        Task<long> CountArticleCommentVotesAsync(ObjectId articleId, ObjectId commentId, VoteType voteType, CancellationToken cancellationToken);
+        Task<T> FindArticleCommentVoteAsync(ObjectId commentId, CancellationToken cancellationToken);
+        Task<long> CountArticleCommentVotesAsync(ObjectId commentId, VoteType voteType, CancellationToken cancellationToken);
         Task<OperationResult> CreateArticleCommentVoteAsync(T entity, CancellationToken cancellationToken);
         Task<OperationResult> UpdateArticleCommentVoteAsync(ObjectId voteId, VoteType voteType, CancellationToken cancellationToken);
         Task<OperationResult> DeleteArticleCommentVoteAsync(ObjectId voteId, CancellationToken cancellationToken);
