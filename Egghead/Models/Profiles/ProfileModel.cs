9633bda4c3460d7dec5efeb903fd8e56990e0a33
victor.chicu <victor.chicu@hotmail.com>
2018-10-10 23:52:33 +0300

    Egghead: Added image path to profile model.

--- Egghead/Models/Profiles/ProfileModel.cs
+++ Egghead/Models/Profiles/ProfileModel.cs
@@ -2,7 +2,9 @@
 {
     public class ProfileModel
     {
+        public string Id { get; set; }
         public string Name { get; set; }
+        public string ImagePath { get; set; }
         public string ArticlesCount { get; set; }
         public string FollowingCount { get; set; }
     }
