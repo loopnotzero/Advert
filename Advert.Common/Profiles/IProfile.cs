5924b8a85f55a87cc821783e6d2125d59b3dcfdd
victor.chicu <victor.chicu@hotmail.com>
2018-12-29 10:44:16 +0200

    Advert.Common: Added profile interface

---
+++ Advert.Common/Profiles/IProfile.cs
@@ -0,0 +1,22 @@
+using System;
+using MongoDB.Bson;
+
+namespace Advert.Common.Profiles
+{
+    public interface IProfile
+    {
+        string Name { get; set; }    
+        string Email { get; set; }
+        string Culture { get; set; }
+        string Location { get; set; }
+        string ImagePath { get; set; }
+        string PhoneNumber { get; set; }  
+        string NormalizedName { get; set; }
+        string NormalizedEmail { get; set; }
+        string LocationNormalized { get; set; }          
+        ObjectId Id { get; set; }           
+        DateTime CreatedAt { get; set; }
+        DateTime ChangedAt { get; set; }  
+        DateTime DeletedAt { get; set; }    
+    }
+}
