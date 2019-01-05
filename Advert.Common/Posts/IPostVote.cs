ca5539a97b3330dd9dbdf573bc3de8a34283fd43
victor.chicu <victor.chicu@hotmail.com>
2019-01-05 02:43:39 +0200

    Advert.Common: Added profile id to post vote interface.

--- Advert.Common/Posts/IPostVote.cs
+++ Advert.Common/Posts/IPostVote.cs
@@ -6,10 +6,9 @@ namespace Advert.Common.Posts
 {
     public interface IPostVote
     {
-        string Email { get; set; }
-        string NormalizedEmail { get; set; }
         ObjectId Id { get; set; }
         ObjectId PostId { get; set; }
+        ObjectId ProfileId { get; set; }
         VoteType VoteType { get; set; }
         DateTime CreatedAt { get; set; }
         DateTime UpdatedAt { get; set; }
