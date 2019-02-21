102e06d6d04a8d896bd6256857bba30b87ef9fce
brian.kernighan <victor.chicu@hotmail.com>
2019-02-21 21:53:29 +0200

    Bazaar.Common: Minor changes for post photos object.

--- Bazaar.Common/Posts/IPostPhotos.cs
+++ Bazaar.Common/Posts/IPostPhotos.cs
@@ -4,14 +4,14 @@ using MongoDB.Bson;
 
 namespace Bazaar.Common.Posts
 {
-    public interface IPostPhoto
+    public interface IPostPhotos
     {
         ObjectId _id { get; set; }
-        string PhotoPath { get; set; }
         string IdentityName { get; set; }
         ObjectId PostId { get; set; }
         DateTime CreatedAt { get; set; }
         DateTime? UpdatedAt { get; set; }
         DateTime? DeletedAt { get; set; }
+        List<string> PhotoPaths { get; set; }        
     }
 }
--- Bazaar.Common/Stores/IPostsPhotosStore.cs
+++ Bazaar.Common/Stores/IPostsPhotosStore.cs
@@ -7,12 +7,12 @@ using MongoDB.Bson;
 
 namespace Bazaar.Common.Stores
 {
-    public interface IPostsPhotosStore<T> : IDisposable where T : IPostPhoto
+    public interface IPostsPhotosStore<T> : IDisposable where T : IPostPhotos
     {
         Task CreatePostPhotoAsync(T entity, CancellationToken cancellationToken);
         Task DeletePostPhotoById(ObjectId photoId, CancellationToken cancellationToken);
         Task DeletePostPhotosByPostId(ObjectId postId, CancellationToken cancellationToken);
         Task DeletePostPhotoByFileName(string fileName, CancellationToken cancellationToken);
-        Task<List<T>> GetPostPhotosByPostIdAsync(ObjectId postId, CancellationToken cancellationToken);
+        Task<T> GetPostPhotosByPostIdAsync(ObjectId postId, CancellationToken cancellationToken);
     }
 }
